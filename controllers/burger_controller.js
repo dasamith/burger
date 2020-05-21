108ee7bbbb4ba1a6da5ee3c837fe78c71279730b
dasamith <60905198+dasamith@users.noreply.github.com>
2020-05-20 22:06:54 -0400

    update js file

---
+++ controllers/burger_controller.js
@@ -0,0 +1,20 @@
+
+
+module.exports = function (app) {
+
+    app.get('/', function (req, res) {
+        res.render('home');
+    });
+
+    var burgerModel = require('./models/burger')
+
+    //this is our save burger route!!
+    app.post('/saveBurger', function (req, res) {
+        console.log('we hit the save burger route!!', req.body)
+        burgerModel.create(['burgers_name', 'devoured'], [req.body.burger_name, false], function () {
+
+        })
+        // model
+        //model calls orm
+    });
+}
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require("express");
+var exphbs = require('express-handlebars');
+var app = express();
 
-const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 const port = 3000
 
 var connection = require('./config/connection.js')
 
-app.get('/', function (req, res) {
-    res.send('goota do control c!!!!')
-})
+require('./controllers/burger_controller.js')(app)
 
 app.listen(port, function () { console.log(`Example app listening at http://localhost:${port}`) })
