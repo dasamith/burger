df25e993c47349649e2c3178304622caf1ccdd8f
dasamith <60905198+dasamith@users.noreply.github.com>
2020-05-23 22:15:44 -0400

    update route

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,12 +1,35 @@
 
 
 module.exports = function (app) {
+    var burgerModel = require('../models/burger');
 
     app.get('/', function (req, res) {
-        res.render('home');
+        burgerModel.all(function (data) {
+            console.log("this is our data", data);
+
+            res.render('home', { burgers: data });
+        })
+
+
     });
 
-    var burgerModel = require('../models/burger');
+    app.put('/devour/:id', function (req, res) {
+        console.log("we hit the devour route", req.params)
+        var updateObj = { devoured: true }
+        var condition = ' id = ' + req.params.id
+
+
+        burgerModel.update(updateObj, condition, function (data) {
+            console.log("this is our data", data);
+
+            res.render('home', { burgers: data });
+        })
+
+
+    });
+
+
+
 
     //this is our save burger route!!
     app.post('/saveBurger', function (req, res) {
