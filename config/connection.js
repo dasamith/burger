eaaec8ae7335fa58469a618eafe4389baabd2820
dasamith <60905198+dasamith@users.noreply.github.com>
2020-05-28 16:31:16 -0400

    ready for deploy

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,16 @@
 require('dotenv').config()
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.DB_PASS,
-    database: 'burgers_db'
-});
+var connection = mysql.createConnection(
+    process.env.JAWSDB_URL
+    // {
+    // host: 'localhost',
+    // user: 'root',
+    // password: process.env.DB_PASS,
+    // database: 'burgers_db'
+    // }
+
+);
 
 connection.connect(function (err) {
     console.log('wer r connected!!', err)
